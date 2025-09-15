9ac18cdcb43b9a5a12dc418da73befe636aa36f6
Matthew Peterson <matthew251125@icloud.com>
2025-09-15 09:57:35 -0500

    Initial Commit

---
+++ 67/_7App.swift
@@ -0,0 +1,17 @@
+//
+//  _7App.swift
+//  67
+//
+//  Created by Matthew Peterson on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct _7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
